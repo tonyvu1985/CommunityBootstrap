5dce3465cc06583decbfd9acf76c2e39ed8a96d1
Tony Vu <vuductrung2003@gmail.com>
2013-12-11 09:44:15 +1100

    rewrite block Topmenu.phtml

---
+++ app/code/local/Responsive/Page/Block/Html/Topmenu.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Top menu block
+ *
+ * @category    Mage
+ * @package     Mage_Page
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Responsive_Page_Block_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
+{    
+    protected function _getHtml(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
+    {
+        $html = '';
+
+        $children = $menuTree->getChildren();
+        $parentLevel = $menuTree->getLevel();
+        $childLevel = is_null($parentLevel) ? 0 : $parentLevel + 1;
+
+        $counter = 1;
+        $childrenCount = $children->count();
+
+        $parentPositionClass = $menuTree->getPositionClass();
+        $itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
+
+        foreach ($children as $child) {
+
+            $child->setLevel($childLevel);
+            $child->setIsFirst($counter == 1);
+            $child->setIsLast($counter == $childrenCount);
+            $child->setPositionClass($itemPositionClassPrefix . $counter ."tony");
+
+            $outermostClassCode = '';
+            $outermostClass = $menuTree->getOutermostClass();
+
+            if ($childLevel == 0 && $outermostClass) {
+                $outermostClassCode = ' class="' . $outermostClass . '" ';
+                $child->setClass($outermostClass);
+            }
+
+            $html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
+            $html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '><span>'
+                . $this->escapeHtml($child->getName()) . '</span></a>';
+
+            if ($child->hasChildren()) {
+                if (!empty($childrenWrapClass)) {
+                    $html .= '<div class="' . $childrenWrapClass . '">';
+                }
+                $html .= '<ul class="level' . $childLevel . '">';
+                $html .= $this->_getHtml($child, $childrenWrapClass);
+                $html .= '</ul>';
+
+                if (!empty($childrenWrapClass)) {
+                    $html .= '</div>';
+                }
+            }
+            $html .= '</li>';
+
+            $counter++;
+        }
+
+        return $html;
+    }
+
+    
+}
