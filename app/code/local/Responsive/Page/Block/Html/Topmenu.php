a796b3235c8c853f53ef983f1ee2a64122c62748
Tony Vu <vuductrung2003@gmail.com>
2013-12-11 11:36:02 +1100

    top menu

--- app/code/local/Responsive/Page/Block/Html/Topmenu.php
+++ app/code/local/Responsive/Page/Block/Html/Topmenu.php
@@ -2,10 +2,11 @@
 /*
  * Top menu block
  *
- * @category    Mage
- * @package     Mage_Page
- * @author      Magento Core Team <core@magentocommerce.com>
+ * @category    Responsive
+ * @package     Responsive_Page
+ * @author      Tony Vu <vuductrung2003@gmail.com>
  */
+
 class Responsive_Page_Block_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
 {    
     protected function _getHtml(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
@@ -27,11 +28,11 @@ class Responsive_Page_Block_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
             $child->setLevel($childLevel);
             $child->setIsFirst($counter == 1);
             $child->setIsLast($counter == $childrenCount);
-            $child->setPositionClass($itemPositionClassPrefix . $counter ."tony");
+            $child->setPositionClass($itemPositionClassPrefix . $counter);
 
             $outermostClassCode = '';
             $outermostClass = $menuTree->getOutermostClass();
-
+  
             if ($childLevel == 0 && $outermostClass) {
                 $outermostClassCode = ' class="' . $outermostClass . '" ';
                 $child->setClass($outermostClass);
@@ -45,7 +46,7 @@ class Responsive_Page_Block_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
                 if (!empty($childrenWrapClass)) {
                     $html .= '<div class="' . $childrenWrapClass . '">';
                 }
-                $html .= '<ul class="level' . $childLevel . '">';
+                $html .= '<ul class="level' . $childLevel . ' dropdown-menu">';
                 $html .= $this->_getHtml($child, $childrenWrapClass);
                 $html .= '</ul>';
 
@@ -61,5 +62,40 @@ class Responsive_Page_Block_Html_Topmenu extends Mage_Page_Block_Html_Topmenu
         return $html;
     }
 
-    
+    /**
+    * Returns array of menu item's classes
+    *
+    * @param Varien_Data_Tree_Node $item
+    * @return array
+    */
+   protected function _getMenuItemClasses(Varien_Data_Tree_Node $item)
+   {
+       $classes = array();
+
+       $classes[] = 'level' . $item->getLevel();
+       $classes[] = $item->getPositionClass();
+
+       if ($item->getIsFirst()) {
+           $classes[] = 'first';
+       }
+
+       if ($item->getIsActive()) {
+           $classes[] = 'active';
+       }
+
+       if ($item->getIsLast()) {
+           $classes[] = 'last';
+       }
+
+       if ($item->getClass()) {
+           $classes[] = $item->getClass();
+       }
+
+       if ($item->hasChildren()) {
+           $classes[] = 'parent dropdown';
+       }
+
+       return $classes;
+   }
+
 }
