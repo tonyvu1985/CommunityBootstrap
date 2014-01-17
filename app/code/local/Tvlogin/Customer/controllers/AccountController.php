0c829d4254552a0cb83d741535564b8798e3527d
Tony Vu <vuductrung2003@gmail.com>
2014-01-17 17:06:01 +1100

    ajax login

--- app/code/local/Tvlogin/Customer/controllers/AccountController.php
+++ app/code/local/Tvlogin/Customer/controllers/AccountController.php
@@ -7,58 +7,26 @@
 
 require_once 'Mage/Customer/controllers/AccountController.php';
 
-class Tvlogin_Customer_AccountController extends Mage_Customer_AccountController {
-    
-    public function indexAction(){
-        echo 'tonychen sd';
-        //Mage::chenlog(Mage::getBacktrace());
-        return;
-    }
-    public function ajaxloginPostAction(){        
-    echo 'tonychen sd';
-    }
-        
-    public function preDispatch()
-    {
-        // a brute-force protection here would be nice
-
-        if (!$this->getRequest()->isDispatched()) {
+class Tvlogin_Customer_AccountController extends Mage_Customer_AccountController {        
+    public function loginAction(){        
+        if (!$this->getRequest()->isAjax()){
+            parent::loginAction();            
             return;
         }
-
-        $action = $this->getRequest()->getActionName();
-        $openActions = array(
-            'create',
-            'login',
-            'logoutsuccess',
-            'forgotpassword',
-            'forgotpasswordpost',
-            'resetpassword',
-            'resetpasswordpost',
-            'confirm',
-            'confirmation',
-            'ajaxloginpost'
-        );
-        $pattern = '/^(' . implode('|', $openActions) . ')/i';
-
-        if (!preg_match($pattern, $action)) {
-            if (!$this->_getSession()->authenticate($this)) {
-                $this->setFlag('', 'no-dispatch', true);
-            }
-        } else {
-            $this->_getSession()->setNoReferer(true);
-        }
-    }
-    public function LoginAction(){
-        $result = array();      
+        $result = array();
         $session = $this->_getSession();     
         $login = $this->getRequest()->getPost('login');   
         if (!empty($login['username']) && !empty($login['password'])) {
             try {
-                //if ($session->login($login['username'], $login['password'])){
-                //    $result['logined'] = 1;
-                //    $result['user'] = $session->getCustomer()->getName();
-                //}
+                if ($session->login($login['username'], $login['password'])){
+                    $result['logined'] = 1;
+                    $result['user'] = $session->getCustomer()->getName();
+                    
+                    /* refresh new content */
+                    $this->loadLayout();
+                    $toplink = $this->getLayout()->getBlock('top.links')->toHtml();
+                    $result['toplink'] = $toplink;
+                }
 
             // If this user just confirmed registration, it need to send him/her a confirmation email.
             } catch (Exception $e) {
@@ -67,11 +35,8 @@ class Tvlogin_Customer_AccountController extends Mage_Customer_AccountController
         } 
         else{
             // Username or password is wrong.
-        }
-       //$this->getResponse()->setBody($result);
-         //$this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
-        echo "test tonychen<br>";
-         return; 
+        }       
+        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
     }
 }
 
