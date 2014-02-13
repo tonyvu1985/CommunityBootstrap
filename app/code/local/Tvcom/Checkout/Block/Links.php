487a54e2e8123ef29b5c7daf76155d4348d41557
tonyvu <vuductrung2003@gmail.com>
2014-02-13 20:34:10 +1100

    bootstrap

--- app/code/local/Tvcom/Checkout/Block/Links.php
+++ app/code/local/Tvcom/Checkout/Block/Links.php
@@ -11,7 +11,12 @@ class Tvcom_Checkout_Block_Links extends Mage_Checkout_Block_Links{
             
             $text = $this->__('My Cart');            
             $parentBlock->removeLinkByUrl($this->getUrl('checkout/cart'));
-            $parentBlock->addLink('<span class="cart-text"> ' . $text . '</span> <span class="item-count">' . $count . '</span>', 'checkout/cart', $text, true, array(), 50, null, 'class="top-link-cart"');
+            if($count == 0){
+                $parentBlock->addLink('<span class="cart-text"> ' . $text . '</span>', 'checkout/cart', $text, true, array(), 50, null, 'class="top-link-cart"');
+            }
+            else{
+                $parentBlock->addLink('<span class="cart-text"> ' . $text . '</span> <span class="item-count">' . $count . '</span>', 'checkout/cart', $text, true, array(), 50, null, 'class="top-link-cart"');
+            }
         }
         return $this;
     }
