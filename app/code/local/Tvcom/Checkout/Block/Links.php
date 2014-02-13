383881db4b625f5fb993acb0dd55f0a47b6a7ee3
Tony Vu <vuductrung2003@gmail.com>
2014-02-13 17:23:11 +1100

    bootstrap

--- app/code/local/Tvcom/Checkout/Block/Links.php
+++ app/code/local/Tvcom/Checkout/Block/Links.php
@@ -8,14 +8,10 @@ class Tvcom_Checkout_Block_Links extends Mage_Checkout_Block_Links{
         if ($parentBlock && Mage::helper('core')->isModuleOutputEnabled('Mage_Checkout')) {
             $count = $this->getSummaryQty() ? $this->getSummaryQty()
                 : $this->helper('checkout/cart')->getSummaryCount();
-            if ($count >= 1) {
-                $text = $this->__('My Cart (%s)', $count);
-            } else {
-                $text = $this->__('My Cart');
-            }
-
+            
+            $text = $this->__('My Cart');            
             $parentBlock->removeLinkByUrl($this->getUrl('checkout/cart'));
-            $parentBlock->addLink($text, 'checkout/cart', $text, true, array(), 50, null, 'class="top-link-cart"');
+            $parentBlock->addLink('<span class="cart-text"> ' . $text . '</span> <span class="item-count">' . $count . '</span>', 'checkout/cart', $text, true, array(), 50, null, 'class="top-link-cart"');
         }
         return $this;
     }
--- app/code/local/Tvcom/Wishlist/Block/Links.php
+++ app/code/local/Tvcom/Wishlist/Block/Links.php
@@ -5,13 +5,15 @@
  * Description: use bootstrap Badges in the toplinks
  */
 class Tvcom_Wishlist_Block_Links extends Mage_Wishlist_Block_Links{
-   protected function _createLabel($count)
-    {
-        if ($count > 1) {
-            return $this->__('My Wishlist (%d)', $count);
-        } else {
-            return $this->__('My Wishlist');
+    
+     protected function _toHtml(){
+        if ($this->helper('wishlist')->isAllow() && ($this->_getItemCount() != 0)) {
+            $text = $this->_createLabel($this->_getItemCount());
+            $this->_label = '<i class="wishlist-icon fa fa-gift"></i> <span class="wishlist-text">My Wishlist</span> <span class="item-count">' . $this->_getItemCount() . '</span>';
+            $this->_title = "My Wishlist";
+            $this->_url = $this->getUrl('wishlist');
+            return Mage_Page_Block_Template_Links_Block::_toHtml();
         }
-    } 
+    }
 }
 ?>
