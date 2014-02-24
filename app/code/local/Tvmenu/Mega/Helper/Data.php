51d83a2c04a8c0c5a9a7ebddb4c1d60c16c875e7
Tony Vu <vuductrung2003@gmail.com>
2014-02-24 17:19:52 +1100

    Mega Menu

---
+++ app/code/local/Tvmenu/Mega/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Tvmenu_Mega_Helper_Data extends Mage_Core_Helper_Abstract{
+    
+}
+?>
