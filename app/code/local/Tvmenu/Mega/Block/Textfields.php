a8580ff772e17300cd1fb574ec3d1e24c6c4ca18
Tony Vu <vuductrung2003@gmail.com>
2014-05-07 09:19:58 +1000

    add more product for sale list

--- app/code/local/Tvmenu/Mega/Block/Textfields.php
+++ app/code/local/Tvmenu/Mega/Block/Textfields.php
@@ -2,13 +2,13 @@
 /**/
  
 
-class Tvmenu_Mega_Block_Custom extends Mage_Adminhtml_Block_System_Config_Form_Field_Array_Abstract
+class Tvmenu_Mega_Block_Textfields extends Mage_Adminhtml_Block_System_Config_Form_Field_Array_Abstract
 {
     public function __construct()
     {        
-        $this->addColumn('value', array(
+        $this->addColumn('product_sku', array(
             'label' => Mage::helper('adminhtml')->__('Product Sku'),
-            'style' => 'width:120px',
+            'style' => 'width:170px',
         ));
         $this->_addAfter = false;
         $this->_addButtonLabel = Mage::helper('adminhtml')->__('Add Product');
