54115a28e17b56335c35730605148008ec939a5b
Tony Vu <vuductrung2003@gmail.com>
2014-05-10 08:19:12 +1000

    remove guest name

--- app/code/local/Tvcom/Page/Block/Html/Welcome.php
+++ app/code/local/Tvcom/Page/Block/Html/Welcome.php
@@ -16,9 +16,9 @@ class Tvcom_Page_Block_Html_Welcome extends Mage_Page_Block_Html_Welcome
         if (empty($this->_data['welcome'])) {
             if (Mage::isInstalled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
                 $this->_data['welcome'] = $this->__('Hi, %s!', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getName()));
-            } else {
+            } /*else {
                 $this->_data['welcome'] = Mage::getStoreConfig('design/header/welcome');
-            }
+            }*/
         }
 
         return $this->_data['welcome'];
